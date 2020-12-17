24ea092d9977a6c19fca480a994bbb79c0799fcb
unknown <genaeee@gmail.com>
2020-12-17 10:05:39 +0200

    first

---
+++ lab7/labs7.cpp
@@ -0,0 +1,21 @@
+﻿// labs7.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+int numericalPairСounter(int amountOfArgument, ...) передает только инт. 3 точки разное кол - во элем.
+int current, next; //текущее и следующее
+int* ptr = &amountOfArgument; //ptr указатель на первый элемент(кол-во элем)
+*(++ptr); //переход на первый элемент
+for (; amountOfArgument > 1; amountOfArgument--) //цикл прохода по всем элементам
+{
+    current = *ptr;
+    next = *(++ptr);
+    if (current < next) //проверка на то,что предыдущий меньше чем следующий
+        result++;
+}
