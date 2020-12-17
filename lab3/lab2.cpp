da2b50f2868c1f7e8429f8cc17ec6aa6bbe20ca6
unknown <genaeee@gmail.com>
2020-12-17 10:10:53 +0200

    first

---
+++ lab3/lab2.cpp
@@ -0,0 +1,56 @@
+﻿
+#include <iostream>
+
+
+void step4()
+{
+	int first;
+	std::cout << "Veddite chislo,kotoroe preobrazuetsya v 4 stepen`" << std::endl;
+	std::cin >> first;
+	first = first * first;
+	first = first * first;
+	std::cout << first << std::endl;
+}
+
+void step8()
+{
+	int first, second;
+	std::cout << "Veddite chislo,kotoroe preobrazuetsya v 8 stepen`" << std::endl;
+	std::cin >> first;
+	second = first * first;
+	second = second * second;
+	first = second * second;
+	std::cout << first << std::endl;
+}
+
+
+void step6()
+{
+	int first, second, three;
+	std::cout << "Veddite chislo,kotoroe preobrazuetsya v 6 stepen`" << std::endl;
+	std::cin >> first;
+	second = first * first;
+	three = second * second;
+	first = three * second;
+	std::cout << first << std::endl;
+}
+
+void step10()
+{
+	int first, second, three, chetveriy;
+	std::cout << "Veddite chislo,kotoroe preobrazuetsya v 10 stepen`" << std::endl;
+	std::cin >> first;
+	second = first * first;
+	three = second * second;
+	chetveriy = three * three;
+	first = chetveriy * second;
+	std::cout << first << std::endl;
+}
+
+int main()
+{
+	step4();
+	step6();
+	step8();
+	step10();
+}
