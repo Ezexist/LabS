c7bd15c158634fd4c7ae490aac529a9899eba658
Ezexist <genaeee@gmail.com>
2021-02-22 09:17:17 +0200

    first

---
+++ src/1semester/lab4/lab4.cpp
@@ -0,0 +1,45 @@
+﻿
+
+#include <iostream>
+
+const double pi = 3.14;
+
+
+int option(char input, int rad)
+{
+	switch (input)
+	{
+	case 'l':
+		std::cout << "circumference, formula(2piR) =  " << 2 * pi * rad << std::endl;
+		break;
+	case 's':
+		std::cout << "square,  formula(piR^2) =  " << pi * rad * rad << std::endl;
+		break;
+	case 'v':
+		std::cout << "volume, formula(4/3Pi R^3) =  " << 4 / 3 * pi * rad * rad * rad << std::endl;
+		break;
+	default:
+		std::cout << "unknown value" << std::endl;
+	}
+	return input;
+}
+
+int calculation()
+{
+	std::cout << "Enter radius" << '\n';
+	int rad;
+	std::cin >> rad;
+	std::cout << "to find:\n l - circumference` \n s - square \n v - volume \n ";
+	char input;
+	std::cin >> input;
+	return option(input, rad);
+}
+
+
+
+
+int main()
+{
+	calculation();
+	return 0;
+}
